4115f8cc1ec1ad575d7bb9a814fe3aea75f612c1
sehy0121 <sbae3@artic.edu>
2020-03-20 13:29:58 +0900

    clean up abstract domains

---
+++ src/main/java/io/dsub/feedapispring/domain/BaseDomain.java
@@ -0,0 +1,4 @@
+package io.dsub.feedapispring.domain;
+
+public class BaseDomain {
+}
--- src/main/java/io/dsub/feedapispring/domain/Countable.java
+++
@@ -1,5 +0,0 @@
-package io.dsub.feedapispring.domain;
-
-public interface Countable {
-    public int count();
-}
